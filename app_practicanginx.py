68e3ade888bd6bec751688ed6f282a1788287351
Mary_Ruiz <maria.ruizsa@anahuac.mx>
2023-09-11 23:27:18 -0600

    HOLA

--- app_practicanginx.py
+++ app_practicanginx.py
@@ -1,12 +1,22 @@
 from flask import Flask
+import socket
 import requests
 import json
 
 app = Flask(__name__)
 
+counter = 0
+
 @app.route('/')
 def index():
     return 'Practica1 - creacion de una APIUrequest!'
+
+@app.route('/count')
+def count():
+    container_id = socket.gethostname()
+    global counter
+    counter += 1
+    return f'<body> <h1>You clicked this page {counter} times on server: {container_id}<h1> <body>'
 
 @app.route('/holamundo/')
 def holamundo():
@@ -22,3 +32,4 @@ def nginx():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+
